522a44f759d5a429a7652f6d3356088778704cf4
agussrib <asbudoyo@gmail.com>
2023-06-27 11:18:15 +0700

    perbaikan setelah di submit

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,8 +1,11 @@
 const Joi = require('joi');
 
+const currentDate = new Date().getFullYear();
+
 const AlbumPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1900).max(currentDate)
+    .required(),
 });
 
 module.exports = {
